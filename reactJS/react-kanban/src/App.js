93196e478af390a03fd07e41e19abe9787450467
Mateus Eduardo <mateuseduardosilvar@gmail.com>
2022-07-05 12:53:50 -0300

    Update react kanban

---
+++ reactJS/react-kanban/src/App.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+import Navbar from "./components/navbar/Navbar";
+import TaskList from "./components/tasklist/Tasklist";
+
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc + 1;
+  return idAcc;
+};
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    };
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask];
+    });
+  };
+
+  const updateTask = (id, title, state) => {
+    setTasks((existingTasks) => {
+      return existingTasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, title, state };
+        } else {
+          return task;
+        }
+      });
+    });
+  };
+
+  const deleteTask = (id) => {
+    setTasks((existingTasks) => existingTasks.filter((task) => task.id !== id));
+  };
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <TaskList
+          title="Pendente"
+          onAddTask={addTask}
+          taskState="Pendente"
+          tasks={tasks.filter((t) => t.state === "Pendente")}
+          onTaskUpdate={updateTask}
+          onDeleteTask={deleteTask}
+        />
+        <TaskList
+          title="Fazendo"
+          onAddTask={addTask}
+          taskState="Fazendo"
+          tasks={tasks.filter((t) => t.state === "Fazendo")}
+          onTaskUpdate={updateTask}
+          onDeleteTask={deleteTask}
+        />
+        <TaskList
+          title="Completo"
+          onAddTask={addTask}
+          taskState="Completo"
+          tasks={tasks.filter((t) => t.state === "Completo")}
+          onTaskUpdate={updateTask}
+          onDeleteTask={deleteTask}
+        />
+      </div>
+    </div>
+  );
+}
